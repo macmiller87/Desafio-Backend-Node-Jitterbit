ec587868bac547f0bc6cfa612199ec684833f3ed
macmiller87 <macamagolf@gmail.com>
2023-09-04 15:25:39 -0300

    Feat: Documentacao Swagger API

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 import express from "express";
 import cors from "cors";
+import swaggerUi from "swagger-ui-express";
+import swaggerFile from "./swagger.json" assert { type: "json" };
 import { orderRoutes } from "./routes/index.routes.js";
+
 
 export const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use(orderRoutes);
+
+app.use("/api-doc", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
